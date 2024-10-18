53c4659fd2d14375c41277326d3d9cade0da739b
ywf154 <149475544+ywf154@users.noreply.github.com>
2024-10-18 14:11:48 +0300

    Update service.js 抽离api地址到config.js

--- src/api/service.js
+++ src/api/service.js
@@ -1,9 +1,9 @@
 import axios from 'axios'
 import {ElMessage} from 'element-plus'// 假设使用了 Element UI 组件库
-
+import {config} from '@/../config'
 // 创建 Axios 实例
 const service = axios.create({
-baseURL: 'http://localhost:8080',// 修改为正确的 URL
+baseURL: config.serverUrl,// 修改为正确的 URL
   timeout: 5000 // 请求超时时间
 })
 
